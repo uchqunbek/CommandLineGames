2d432be9512b765eccb8f0387099a87816c0e233
Shuhrat <sh.khudayberganov@unicon.uz>
2015-11-27 12:53:17 +0500

    FindNumber tayyor

---
+++ src/uz/unicon/clg/TestMain.java
@@ -0,0 +1,15 @@
+package uz.unicon.clg;
+
+/**
+ * Created by John on 26/11/2015.
+ */
+public class TestMain {
+
+
+    public static void main(String[] args) {
+        FindNumber asd = new FindNumber();
+        asd.run();
+    }
+}
+
+
