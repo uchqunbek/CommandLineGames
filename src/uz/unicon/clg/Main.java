2fc490f442026a0b71c01569fac46bb49f29033a
Aliw <a.ergashev@unicon.uz>
2015-11-27 17:03:44 +0500

    in cycle

--- src/uz/unicon/clg/Main.java
+++ src/uz/unicon/clg/Main.java
@@ -11,6 +11,12 @@ public class Main {
 	    CommandLineGame[] games = new CommandLineGame[] {new FindNumber(), new FindCellNumbers()};
         GameStat gameStat = new GameStat();
 
+        int gamesPlayed = 1;
+        int winNumber = 1;
+
+        while (true){
+
+
         for(int x = 1; x <= games.length; x ++){
             System.out.println(x + ". " + games[x-1].getGameName());
         }
@@ -21,24 +27,21 @@ public class Main {
         int chooseGame = scanner.nextInt();
         System.out.println("Choosen game = " + games[chooseGame].getGameName());
 
-        gameStat.setGamesPlayed(1);
-        gameStat.setWinNumber(1);
+        gameStat.setGamesPlayed(gamesPlayed++);
+        gameStat.setWinNumber(winNumber++);
         games[chooseGame].run();
 
 
         System.out.println("Games Played = " + gameStat.getGamesPlayed());
         System.out.println("Win Number = " + gameStat.getWinNumber());
 
+        }
 
 
-
-//        while (true) {
-//
 //            /**
 //             * TODO: 1. O'yinlar ro'yxatini raqamlangan holda, nomlari bilan ekranga chiqaring va foydalanuvchidan o'yin raqamini kiritishni so'rang. Raqam kiritilgandan so'ng, ushbu raqam ostidagi o'yinni ishga tushiring.
 //             * TODO: 2. O'ynalgan o'yinlar soni va g'alabalar sonini saqlaydigan hisobot olib boruvchi klass yarating va ishga tushiring.
 //             * */
-//        }
 
 
     }
